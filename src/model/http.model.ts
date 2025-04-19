483061ded54bf778e5bed72a6cc2bc44c26a4675
xu_xz <xxz0528@qq.com>
2025-04-19 17:28:02 +0800

    opt: 优化http返回结构

---
+++ src/model/http.model.ts
@@ -0,0 +1,25 @@
+import { XzSafeAny } from "./any.model";
+
+export interface XzHttpResponse{
+  /** 请求是否成功 */
+  success:boolean;
+  /** 状态码 */
+  statusCode?:number;
+  /** 消息 */
+  message:string;
+  /**核心业务数据 */
+  data:XzSafeAny;
+  /** http错误类型名称:Bad Request  */
+  error?:string;
+  /** 自定义错误码（可选，用于前端逻辑处理）如:INVALID_EMAIL */
+  errorCode?:string;
+  /** 详细错误列表（如验证错误） */
+  errors?:XzErrorOption[];
+  /** 请求时间戳 */
+  timestamp?:Date;
+}
+
+export interface XzErrorOption{
+  field:string;
+  message:string;
+}
